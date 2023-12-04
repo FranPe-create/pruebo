030449b211047f47aee67e8753a68ee341ee3ffc
FranPe <petaqui@gmail.com>
2023-12-04 16:47:18 +0100

    modificado

--- probando.py
+++ probando.py
@@ -1,3 +1,5 @@
 #hola esto es una prueba para el proyecto de circleci
 
-print "hola veamos que es lo que tenemos que poner"
+print ("hola veamos que es lo que tenemos que poner")
+print ("hola vamos a modificar")
+print("modificado")
